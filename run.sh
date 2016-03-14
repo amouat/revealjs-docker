f064ca2d896f3a36e2f8bf27d3526259d34ca21a
Adrian Mouat <adrian.mouat@gmail.com>
2016-03-14 12:20:05 +0000

    Update run.sh.

--- run.sh
+++ run.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
-docker run --name revealjs -d -p 8000:8000 -p 35729:35729 -v $PWD/pres:/reveajs/pres amouat/revealjs
+docker rm -f revealjs || true
+docker run --name revealjs -d -p 8000:8000 -p 35729:35729 -v $PWD/pres:/revealjs/pres amouat/revealjs
